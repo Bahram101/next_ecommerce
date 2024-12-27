132b018e8de9836c1b6e43371e8204c059d583c2
Bahrom <bahram101@mail.ru>
2024-12-27 18:47:38 +0500

    mode toggle

---
+++ components/shared/header/mode-toggle.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { DropdownMenu } from "@radix-ui/react-dropdown-menu";
+import { useTheme } from "next-themes";
+
+const ModeToggle = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
+};
+
+export default ModeToggle;
